df857874bf8b8c1f3ef1ccc4b063001eb3bf82f0
Nils <mail@nils.cc>
2026-05-19 12:29:23 +0200

    ploopy: nilscc keymap update with custom mouse/drag scroll layer

--- keyboards/ploopyco/trackball/keymaps/nilscc/keymap.c
+++ keyboards/ploopyco/trackball/keymaps/nilscc/keymap.c
@@ -1,20 +1,3 @@
-/* Copyright 2020 Christopher Courtney, aka Drashna Jael're  (@drashna) <drashna@live.com>
- * Copyright 2019 Sunjun Kim
- * Copyright 2020 Ploopy Corporation
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
 #include QMK_KEYBOARD_H
 
 enum layers {
@@ -24,55 +7,59 @@ enum layers {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [BASE] = LAYOUT( /* Base */
-        KC_BTN1, KC_BTN3, KC_BTN2, KC_BTN4, MO(RAISE)
+        KC_BTN1, KC_BTN3, KC_BTN2, MO(RAISE), XXXXXXX
     ),
 
     [RAISE] = LAYOUT(
-        KC_BTN1, KC_BTN3, KC_BTN2, KC_BTN5, XXXXXXX
+        KC_BTN4, KC_BTN3, KC_BTN5, XXXXXXX, XXXXXXX
     ),
 };
 
 static bool scrolling_mode = false;
 
+static const int16_t DEFAULT_CPI = 900;
+
+
 layer_state_t layer_state_set_user(layer_state_t state) {
     switch (get_highest_layer(state)) {
         case RAISE:  // If we're on the RAISE layer enable scrolling mode
             scrolling_mode = true;
-            pointing_device_set_cpi(200);
+            // pointing_device_set_cpi(40);
             break;
         default:
             if (scrolling_mode) {  // check if we were scrolling before and set disable if so
                 scrolling_mode = false;
-                pointing_device_set_cpi(800);
+                // pointing_device_set_cpi(900);
             }
             break;
     }
     return state;
 }
 
-// Modify these values to adjust the scrolling speed
-#define SCROLL_DIVISOR_H 8.0
-#define SCROLL_DIVISOR_V 8.0
-
-// Variables to store accumulated scroll values
-static float scroll_accumulated_h = 0;
-static float scroll_accumulated_v = 0;
-
 report_mouse_t pointing_device_task_user(report_mouse_t mouse_report) {
     if (scrolling_mode) {
-        // Calculate and accumulate scroll values based on mouse movement and divisors
-        scroll_accumulated_h += (float)mouse_report.x / SCROLL_DIVISOR_H;
-        scroll_accumulated_v -= (float)mouse_report.y / SCROLL_DIVISOR_V;
 
-        // Assign integer parts of accumulated scroll values to the mouse report
-        mouse_report.h = (int8_t)scroll_accumulated_h;
-        mouse_report.v = (int8_t)scroll_accumulated_v;
+        // higher scale slower scrolling
+        static const int16_t scale = 32;
 
-        // Update accumulated scroll values by subtracting the integer parts
-        scroll_accumulated_h -= (int8_t)scroll_accumulated_h;
-        scroll_accumulated_v -= (int8_t)scroll_accumulated_v;
+        static int16_t acc_h = 0;
+        static int16_t acc_v = 0;
 
-        // Clear the X and Y values of the mouse report
+        // axis snapping
+        if (abs(mouse_report.x) > abs(mouse_report.y)) {
+            acc_h += mouse_report.x;
+            mouse_report.h = acc_h / scale;
+            mouse_report.v = 0;
+            acc_h %= scale;
+        } else {
+            acc_v -= mouse_report.y;
+            mouse_report.h = 0;
+            mouse_report.v = acc_v / scale;
+            acc_v %= scale;
+        }
+
+
+        // clear the X and Y values of the mouse report
         mouse_report.x = 0;
         mouse_report.y = 0;
     }
